ec8cabdf44f5b78ee1a118f4caa98e437632c982
sepaww <117186747+sepaww@users.noreply.github.com>
2023-02-25 14:12:04 +0200

    Update visuals.py

--- nsrc/ui/visuals.py
+++ nsrc/ui/visuals.py
@@ -21,6 +21,8 @@ class Renderer():
         self.light_bg="#ab6b33"
         self.dark_bg="#5c3614"
     def bg_col_picker(self, i, j):
+        """generates the zigzag color pattern
+        """
         if i%2==0:
             if j%2==0:
                 return self.light_bg
@@ -31,10 +33,14 @@ class Renderer():
             return self.light_bg
         
     def tile_draw(self, height, width):
+        """ draws the square in the given cords with correct color
+        """
         col=self.bg_col_picker(height, width)
         pygame.draw.rect(self.screen, col,[width*self.tile_size, height*self.tile_size, self.tile_size, self.tile_size])
     
     def piece_draw(self, piece_id, team_id, height, width):
+        """ draws the correct piece based on id, team and cords given. Draws the piece based on the matrix given in self.sprite_list
+        """
         def sprite_draw(i, j, col):
             pygame.draw.rect(self.screen, col,[(width)*self.tile_size+j*self.sprite_size+10, (height)*self.tile_size+i*self.sprite_size+10, self.sprite_size, self.sprite_size])
         if team_id==self.starter:
@@ -54,6 +60,8 @@ class Renderer():
                 j+=1
             i+=1
     def whole_update_screen(self):
+        """ draws the entire chessboard
+        """
         i,j=0,0
         for row in self.game_matrix:
             j=0
@@ -72,9 +80,13 @@ class Renderer():
     
         
     def draw_possible_moves(self, move):
+        """ draws a centered circle in the given cords
+        """
         pygame.draw.circle(self.screen, "#363433",[int(move[1]*self.tile_size+self.tile_size/2), int(move[0]*self.tile_size+self.tile_size/2)], int(self.tile_size/8))
         
     def reset_tile(self, cords):
+        """ used to remove move indicators from board or to update wanted tiles adter move
+        """
         i=cords[0]
         j=cords[1]
         tile=self.game_matrix[i][j]
@@ -85,6 +97,8 @@ class Renderer():
         else:
             self.tile_draw(i, j)
     def update_screen(self):
+        """used to update the screen or to give response to pygame so that it doesnt keep freezing (annoying)
+        """
         pygame.display.update()
         
     
@@ -95,6 +109,7 @@ class Renderer():
         #long list for chess pieces
         self.sprite_list=[
             0,
+            #PAWN
             [
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -107,6 +122,7 @@ class Renderer():
                 [0, 0, 2, 1, 1, 1, 1, 1, 0, 0],
                 [0, 0, 2, 1, 1, 1, 1, 1, 0, 0],
             ],
+            #BISHOP
             [
                 [0, 0, 0, 2, 0, 2, 1, 0, 0, 0],
                 [0, 0, 0, 2, 0, 1, 1, 0, 0, 0],
@@ -119,6 +135,7 @@ class Renderer():
                 [0, 0, 2, 1, 1, 1, 1, 1, 0, 0],
                 [0, 2, 1, 1, 1, 1, 1, 1, 1, 0],
             ],
+            #KNIGHT
             [
                 [0, 0, 0, 0, 2, 0, 2, 0, 0, 0],
                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
@@ -131,6 +148,7 @@ class Renderer():
                 [0, 0, 0, 2, 1, 1, 1, 0, 0, 0],
                 [0, 0, 2, 1, 1, 1, 1, 1, 0, 0],
             ],
+            #ROOK
             [
                 [0, 2, 2, 0, 2, 2, 0, 2, 2, 0],
                 [0, 2, 1, 1, 1, 1, 1, 1, 1, 0],
@@ -143,6 +161,7 @@ class Renderer():
                 [0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
                 [0, 2, 1, 0, 0, 0, 0, 1, 1, 0],
             ],
+            #QUEEN
             [
                 [0, 0, 2, 0, 2, 1, 0, 2, 0, 0],
                 [0, 2, 0, 0, 2, 1, 0, 0, 1, 0],
@@ -155,6 +174,7 @@ class Renderer():
                 [0, 0, 2, 1, 1, 1, 1, 1, 0, 0],
                 [0, 2, 1, 1, 1, 1, 1, 1, 1, 0],
             ],
+            #KING
             [
                 [2, 0, 0, 0, 2, 1, 0, 0, 0, 2],
                 [0, 2, 0, 2, 1, 1, 1, 0, 2, 0],
